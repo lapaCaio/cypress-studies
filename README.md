6f62ef1004cdc40da54d93a6a995a916c1f4a321
lapaCaio <lapacaio2001@gmail.com>
2024-07-29 09:04:52 -0300

    ADD

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# cypress-studies
- ...
+# Boas práticas em automação de testes com Cypress
+
+👋 Seja bem vinda(o)!
+
+As más práticas em questão são as seguintes:
+
+- _Browser testing_
+- Duplicação de código
+- _Flaky tests_
+- _Hardcoded assertions_
+- Complexidade desnecessária
+- _Page Objects_
+- Dados sensíveis versionados
+- Testes lentos
+- Dependência entre testes
+- Abstrações erradas
+
+## Vamos começar?
+
+Vá para a seção de [pré-requisitos](./lessons/0.md).
---
+++ cypress/e2e/duplication/sample2.cy.js
@@ -0,0 +1,30 @@
+describe('Code duplication bad practice - repetitive tests', () => {
+  beforeEach(() => {
+    cy.intercept(
+      'GET',
+      '**/search**'
+    ).as('getStories')
+
+    cy.visit('https://hackernews-seven.vercel.app')
+    cy.wait('@getStories')
+
+    cy.get('input[type="text"]')
+      .should('be.visible')
+      .and('have.value', 'redux')
+      .as('searchField')
+      .clear()
+  })
+
+  const termsToSearchFor = ["reactjs", "vuejs", "angularjs"]
+
+  termsToSearchFor.forEach(term => {
+    it('searches for' + term + "", () => {
+      cy.search(term)
+
+      cy.wait('@getStories')
+
+      cy.get('.table-row')
+        .should('have.length', 100)
+    })
+  })
+})
